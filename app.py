1d5565f41b5e4f42cd544a50d35a94e2c1b02272
kumarsub.iyer@gmail.com <kumar.iyer@gatech.edu>
2026-06-07 18:21:27 -0400

    Check in app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return {"message": "Hello from Kumar to try a Flask app in Vercel!"}
+    return "Hello from Kumar to try a Flask app in Vercel!"
 
 @app.route('/about')
 def about():
