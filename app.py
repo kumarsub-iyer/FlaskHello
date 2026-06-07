21e090b34bd2d4d4f36c1f08eeb8abafb5a2044d
kumarsub.iyer@gmail.com <kumar.iyer@gatech.edu>
2026-06-07 18:46:37 -0400

    Add login route and template rendering to app.py

--- app.py
+++ app.py
@@ -7,6 +7,14 @@ app = Flask(__name__)
 def home():
     return "Hello from Kumar to try a Flask app in Vercel!"
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        name = request.form['username']
+        return f"Hello {name}, POST request received"
+    return render_template('name.html')
+
+
 @app.route('/about')
 def about():
     return {"status": "success", "page": "about"}
